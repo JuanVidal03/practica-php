f959dd28d4fcd5457ae25b541f3625ceeceac171
Juan Vidal <juanmvg2003@gmail.com>
2024-05-09 22:47:18 -0500

    metodo para eliminar

--- productosDAO.php
+++ productosDAO.php
@@ -8,7 +8,6 @@
         public $descripcion;
         public $precio;
         public $stock;
-        
 
         public function __construct($id, $titulo, $descripcion, $precio, $stock) {
             $this->id = $id;
@@ -18,12 +17,12 @@
             $this->stock = $stock;
         }
 
+
         // obteniedno todos los productos
         public function getAllProducts(){
-
             try {
                 
-                $instancia = new Connection('localhost', 'root', '', 'php-test');
+                $instancia = new Connection('localhost', 'root', '', 'php_test');
     
                 $conexion = $instancia->conectar();
                 $query = $conexion->prepare("SELECT * FROM productos");
@@ -33,15 +32,30 @@
                 print_r(json_encode($productos));
                 
                 // desconectar la conexion
-                $desconectar = $instancia->desconectar();
+                $instancia->desconectar();
                 
             } catch (PDOException $e) {
                 echo "Algo ha salido mal: {$e}";
             }
         }
-        
 
-        
+
+        // eliminar un producto
+        public function deleteProduct(int $id){
+            try {
+
+                $instancia = new Connection('localhost','root', '', 'php_test');
+                $conexion = $instancia->conectar();
+                $query = $conexion->prepare("DELETE FROM productos WHERE id = {$id}");
+                $query->execute();
+                
+                print_r(json_encode("Producto eliminado con exito!"));
+
+            } catch(PDOException $e){
+                echo "Error al eliminar producto: {$e->getMessage()}";
+            }
+        }
+
     }
 
 ?>
