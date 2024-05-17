72af0565ddf22c7bb69f175f9f5895a04e8c3aaa
Juan Vidal <juanmvg2003@gmail.com>
2024-05-17 07:27:00 -0500

    metodo eliminar funcionando

--- controllers/allProductosController.php
+++ controllers/allProductosController.php
@@ -1,10 +1,9 @@
 <?php
 
     include('../models/productosDAO.php');
-    $productoDAO = new ProductoDAO();
-    $productos = $productoDAO->getAllProducts();
-    die();
 
-    return json_encode($productos);
+    $productoDAO = new ProductoDAO();
+    $productos = $productoDAO->getAllProducts(); 
+    print_r(json_encode($productos));
 
 ?>
--- main.js
+++ main.js
@@ -1,25 +1,51 @@
 // don elements
 const tbody = document.querySelector("#body");
 
-const fetchData = async() => {
-    const res = await fetch("./controllers/allProductosController.php");
-    const data = await res.json();
+// renderizando productos
+const allProducts = async() => {
+    try {
+        
+        const res = await fetch("./controllers/allProductosController.php");
+        const data = await res.json();
+    
+        data.forEach(producto => {
+            tbody.innerHTML += `
+                <tr>
+                    <th class="text-center">${producto.id}</th>
+                    <td class="text-center">${producto.titulo}</td>
+                    <td class="text-center">${producto.descripcion}</td>
+                    <td class="text-center">$${producto.precio} COP</td>
+                    <td class="text-center">${producto.stock}</td>
+                    <td class="text-center">
+                        <i
+                            style="margin-right: 1rem;"
+                            class="fa-solid fa-pen-to-square text-primary"
+                        ></i>
+                        <i
+                            class="fa-solid fa-trash text-danger"
+                            onclick="deleteProduct(${producto.id})"
+                        ></i>
+                    </td>
+                </tr>
+            `;
+        });
 
-    data.forEach(producto => {
-        tbody.innerHTML += `
-            <tr>
-                <th class="text-center">${producto.id}</th>
-                <td class="text-center">${producto.titulo}</td>
-                <td class="text-center">${producto.descripcion}</td>
-                <td class="text-center">$${producto.precio} COP</td>
-                <td class="text-center">${producto.stock}</td>
-                <td class="text-center">
-                    <i style="margin-right: 1rem;" class="fa-solid fa-pen-to-square text-primary"></i>
-                    <i class="fa-solid fa-trash text-danger"></i>
-                </td>
-            </tr>
-        `;
-    });
+    } catch (error) {
+        console.log(`Error al renderizar los productos: ${error.message}`);
+    }
 }
+allProducts();
 
-fetchData();
+// eliminar producto
+const deleteProduct = async(id) => {
+    try {
+
+        const res = await fetch(`./controllers/deleteProductController.php?id=${id}`, {
+            method: 'DELETE'
+        });
+        location.reload();
+
+    } catch (error) {
+        console.log(`Error al eliminar producto: ${error}`);
+    }
+}
