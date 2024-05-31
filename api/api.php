6c80c567848b432982e432e1f1c3e5c35c703046
Juan Vidal <juanmvg2003@gmail.com>
2024-05-31 08:07:59 -0500

    header para acceder a la informacion

--- api/api.php
+++ api/api.php
@@ -7,6 +7,8 @@
     // tipo de data que puede recibir
     header("Content-Type: application/json");
     header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Methods: GET, POST, DELETE"); 
+    header("Access-Control-Allow-Headers: Content-Type, Authorization"); 
     // obteniendo el metodo de la peticion
     $method = $_SERVER["REQUEST_METHOD"];
 
